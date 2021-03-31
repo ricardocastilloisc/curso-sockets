dab601aa9f5fdf43a8e983b5994ab3be68fd6c85
ricardocastillo <ricardocastilloisc@gmail.com>
2021-03-30 21:19:43 -0500

    43. Reconectar y renombrar usuario en el Socket Server

--- 2basico/src/app/services/websocket.service.ts
+++ 2basico/src/app/services/websocket.service.ts
@@ -61,9 +61,8 @@ export class WebsocketService {
 
   cargarStorage = () =>{
     if(localStorage.getItem('usuario')) {
-
       this.usuario = JSON.parse(localStorage.getItem('usuario'));
-
+      this.loginWs(this.usuario.nombre);
     }
   }
 }
